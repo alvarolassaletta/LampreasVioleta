614a8528f3930cae17595debf6309f3df3790503
alvaro <alvarolassaletta@gmail.com>
2026-01-03 20:13:34 +0100

    refactor: update ObjectMapper

--- src/main/java/services/JsonIO.java
+++ src/main/java/services/JsonIO.java
@@ -2,6 +2,7 @@ package services;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.File;
@@ -15,9 +16,20 @@ import java.io.IOException;
 public final class JsonIO {
 
     // ObjectMapper es el motor de Jackson: convierte Java <-> JSON
+
     private static final ObjectMapper MAPPER = new ObjectMapper()
+            // Permite que Jackson maneje correctamente las fechas y horas de Java 8+
+            .registerModule(new JavaTimeModule())
+            //Evita que las fechas se serialicen como números (timestamps)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .enable(SerializationFeature.INDENT_OUTPUT); // JSON "bonito" (pretty print)
 
+    /*JavaTimeModule())
+    *  Para que no provoque problemas de compilación:
+    * --La version de jackson-databind y jackson-datatype-jsr310 tiene que ser la misma
+    * --El compilador de maven y el del IDE ha de ser el mismo
+    *   Project structure -> Sdk -> 17
+    * */
     private JsonIO() { }
 
     /** Escribe un objeto Java como JSON en el fichero. */
